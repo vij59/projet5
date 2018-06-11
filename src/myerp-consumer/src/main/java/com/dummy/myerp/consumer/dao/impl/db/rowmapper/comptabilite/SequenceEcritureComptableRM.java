e44d2556ed4ea51f8f82a66708b14d7193193ed0
edvigier <edouard.vigier@capgemini.com>
2018-06-11 19:46:52 +0200

    myerp modifié, addreference en cours, carnage bis

---
+++ src/myerp-consumer/src/main/java/com/dummy/myerp/consumer/dao/impl/db/rowmapper/comptabilite/SequenceEcritureComptableRM.java
@@ -0,0 +1,19 @@
+package com.dummy.myerp.consumer.dao.impl.db.rowmapper.comptabilite;
+
+import com.dummy.myerp.model.bean.comptabilite.SequenceEcritureComptable;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SequenceEcritureComptableRM implements RowMapper<SequenceEcritureComptable> {
+
+    @Override
+    public SequenceEcritureComptable mapRow(ResultSet pRS, int pRowNum) throws SQLException {
+        SequenceEcritureComptable vBean = new SequenceEcritureComptable();
+        vBean.setAnnee(pRS.getInt("annee"));
+        vBean.setDerniereValeur(pRS.getInt("derniere_valeur"));
+
+        return vBean;
+    }
+}
